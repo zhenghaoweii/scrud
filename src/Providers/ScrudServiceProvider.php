d690fc0b6e63ee0d7a2166d156e7dc23df5f58dd
zhenghaoweii <zhenghaowei88@gmail.com>
2020-04-11 01:28:27 +0700

    Update service provider

--- src/Providers/ScrudServiceProvider.php
+++ src/Providers/ScrudServiceProvider.php
@@ -3,11 +3,18 @@
 namespace limitless\scrud\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use limitless\scrud\Commands\ApiGenerator;
+use limitless\scrud\Commands\SyncTableColumns;
 
 class ScrudServiceProvider extends ServiceProvider
 {
     public function boot(){
-
+        if ($this->app->runningInConsole()) {
+            $this->commands([
+                    ApiGenerator::class,
+                    SyncTableColumns::class,
+            ]);
+        }
     }
 
     public function register()
