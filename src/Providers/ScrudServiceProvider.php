ee900cf63a9ced14a628cee7a1681cd788223ca6
zhenghaowei <zhenghaowei88@gmail.com>
2020-04-11 00:50:58 +0700

    Init Packages

---
+++ src/Providers/ScrudServiceProvider.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace limitless\scrud\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class ScrudServiceProvider extends ServiceProvider
+{
+    public function boot(){
+
+    }
+
+    public function register()
+    {
+        parent::register();
+    }
+}
